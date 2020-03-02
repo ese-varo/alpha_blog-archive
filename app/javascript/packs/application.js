ea2d49a2604b375c16ccfc11d090a0a5b6802dbe
Alvaro <varoboss.ap@gmail.com>
2020-03-01 22:02:44 -0600

    Create homepage and add links

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,13 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import './bootstrap_custom.js'
+import '../stylesheets/application'
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
